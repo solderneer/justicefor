b451c954e7d5377248af8e3601fe0e37f9ddeb27
Sudharshan <solderneer@gmail.com>
2020-06-08 22:47:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var mailtoAshanti = `
+mailto:countyexecutive@co.pg.md.us,MFRiddick@co.pg.md.us,MAMagaw@co.pg.md.us?subject=Justice%20for%20Ashanti%20Carmon&body=Dear%20sir%2Fmadam%2C%0D%0A%0D%0AMy%20name%20is%20%5Binsert%20name%5D%2C%20and%20I%20am%20emailing%20today%20to%20demand%20further%20action%20for%20the%20murder%20of%20Ashanti%20Carmon.%0D%0A%0D%0AAshanti%20Carmon%20was%20a%20trans%20woman%20with%20her%20whole%20life%20ahead%20of%20her.%20On%20the%20eve%20of%20the%20Transgender%20Day%20of%20Visibility%20in%202019%2C%20Ashanti%20and%20her%20fianc%C3%A9%20went%20for%20a%20movie%20and%20dinner%20together.%20Her%20fianc%C3%A9%20then%20left%20for%20work%20and%20she%20went%20out%20with%20her%20friends.%20In%20the%20early%20hours%20of%20March%2031st%2C%20her%20body%20was%20found%20near%20DC's%20city%20limits%2C%20where%20she%20was%20pronounced%20dead%20at%20the%20scene.%20Nobody%20has%20been%20named%20a%20suspect%20in%20this%20investigation.%20A%20similar%20case%20occurred%20only%20miles%20away%20from%20the%20site%20of%20Ashanti's%20death%3B%20Zoe%20Spears%2C%20Ashanti%20Carmon's%20dear%20friend%20and%20fellow%20trans%20woman%20of%20color%2C%20was%20shot%20and%20killed%20as%20well.%0D%0A%0D%0ATransgender%20women%2C%20particularly%20those%20of%20color%2C%20face%20disproportionately%20high%20levels%20of%20violence.%20They%20deserve%20the%20same%20degree%20of%20protection%20as%20anyone%20else.%20Ashanti's%20death%20is%20unfortunately%20just%20one%20example%20in%20thousands%20of%20cases%20of%20hate%20crimes%20against%20transgender%20women%20of%20color.%0D%0A%0D%0AI%20urge%20you%20to%20continue%20the%20investigation%20into%20the%20death%20of%20Ashanti%20Carmon%20and%20deliver%20justice%20for%20her%20unwarranted%20and%20gruesome%20death.%0D%0A%0D%0ASincerely%2C%0D%0A%5Byour%20name%5D%0D%0A
+`
+
+var mailtoBailey = `
+mailto:johnnyo@baltimorecountymd.gov?subject=JUSTICE%20FOR%20BAILEY%20REEVES&body=Dear%20sir%2Fmadam%2C%0D%0A%0D%0AMy%20name%20is%20(your%20name)%20and%20I%20am%20writing%20to%20you%20to%20demand%20justice%20for%20Bailey%20Reeves%2C%20a%20black%20transgender%20woman%20that%20was%20fatally%20shot%20over%20the%20Labor%20Day%20weekend%20in%20Maryland.%20Her%20body%20was%20found%20on%20September%20the%202nd.%20She%20was%20only%2017.%0D%0A%0D%0AWhen%20transphobia%20mixes%20with%20misogyny%20and%20racism%2C%20it%20results%20in%20fatal%20violence.%20This%20amount%20of%20prejudice%20is%20unacceptable.%20Trans%20women%20of%20color%20face%20a%20disproportionately%20high%20level%20of%20violence%20against%20them%2C%20leading%20to%20the%20statistic%20that%20most%20hate%20crimes%20committed%20against%20trans%20individuals%20are%20against%20black%20trans%20women.%20I%20demand%20that%20you%20continue%20the%20investigation%20into%20Bailey%20Reeves%E2%80%99%20death%20and%20ensure%20that%20justice%20is%20rightly%20delivered.%0D%0A%0D%0ARegards%2C%0D%0A(Your%20name)%0D%0A%0D%0A
+`
+
+var mailtoBee = `
+mailto:bocc1@hendryfla.net,bocc3@hendryfla.net?subject=Justice%20for%20Bee%20Love%20Slater&body=Dear%20sir%2Fmadam%2C%0D%0A%0D%0AMy%20name%20is%20%5Byour%20name%5D%20and%20I%20am%20writing%20to%20you%20with%20a%20plea%20to%20continue%20the%20investigation%20into%20the%20death%20of%20Bee%20Love%20Slater.%0D%0A%0D%0AOn%20September%204th%2C%202019%2C%20Bee%20Love%20Slater%20was%20found%20immolated%20in%20the%20flaming%20remains%20of%20what%20was%20her%20car.%20Her%20body%20was%20charred%20beyond%20recognition%2C%20and%20the%20only%20way%20she%20could%20be%20identified%20was%20through%20the%20car%E2%80%99s%20number%20plate.%20There%20were%20also%20signs%20of%20struggle%20on%20her%20body.%0D%0A%0D%0AFlorida%E2%80%99s%20hate%20crime%20laws%20do%20not%20extend%20protection%20to%20transgender%20individuals%20as%20the%20hate%20crime%20statute%20does%20not%20extend%20its%20protection%20to%20those%20who%20are%20attacked%20for%20their%20gender%20identity%2C%20and%20yet%20trans%20women%20of%20color%20are%20one%20of%20the%20most%20marginalized%20communities.%20They%20deserve%20to%20feel%20safe%20in%20their%20neighborhood.%20Please%20ensure%20that%20justice%20is%20delivered%3B%20for%20the%20sake%20of%20Bee%20Love%20Slater%20and%20for%20the%20sake%20of%20the%20larger%20black%20transgender%20community.%0D%0A%0D%0AYours%20sincerely%2C%0D%0A%5BYour%20name%5D%0D%0A%0D%0A-------------------------------------------------------------------------------------------------------------------------------%0D%0A%0D%0AIF%20YOU%20RESIDE%20IN%20THE%20USA%2C%20PLEASE%20USE%20THE%20FOLLOWING%20LINK%20TO%20CONTACT%20SENATOR%20MARCO%20RUBIO%20TO%20UPDATE%20THE%20STATUTE%20THAT%20DOES%20NOT%20EXTEND%20ITS%20PROTECTION%20TO%20THOSE%20ATTACKED%20FOR%20THEIR%20GENDER%20IDENTITY%3A%0D%0A%0D%0Ahttps%3A%2F%2Fwww.rubio.senate.gov%2Fpublic%2Findex.cfm%2Fcontact%0D%0A%0D%0A
+`
+
+var mailtoMonika = `
+mailto:george.dunlap@mecklenburgcountync.gov,elaine.powell@mecklenburgcountync.gov?subject=Justice%20for%20Monika%20Diamond&body=Dear%20sir%2Fmadam%2C%0D%0A%0D%0AMy%20name%20is%20(your%20name)%20and%20I%20am%20writing%20to%20you%20concerning%20the%20death%20of%20Monika%20Diamond%2C%20a%20transgender%20woman%20who%20was%20shot%20to%20death%20in%20your%20county.%0D%0A%0D%0AMonika%20Diamond%20was%20an%20activist%20and%20a%20businesswoman.%20She%20was%20a%20joint-executive%20in%20a%20pageant%20that%20empowered%20LGBTQ%2B%20mothers.%20She%20was%20a%20brilliant%20woman%20that%20did%20not%20deserve%20to%20have%20her%20life%20taken%20away%20from%20her.%0D%0A%0D%0AThere%20is%20an%20epidemic%20of%20violence%20against%20transgender%20women%20of%20color%2C%20and%20the%20issue%20starts%20with%20the%20fact%20that%20North%20Carolina%E2%80%99s%20hate%20crime%20legislation%20does%20not%20cover%20crimes%20committed%20against%20transgender%20or%20gender-nonconforming%20individuals%20in%20places%20such%20as%20their%20venue%20of%20employment%2C%20housing%2C%20and%20public%20spaces.%20We%20need%20to%20do%20better.%0D%0A%0D%0AYour%20police%20department%20and%20local%20officials%20have%20given%20out%20the%20incorrect%20name%20for%20Ms.%20Diamond%20and%20misgendered%20her%20countless%20times%2C%20leading%20to%20confusion%20in%20the%20media.%20This%20is%20also%20highly%20disrespectful%20to%20her%20name%20and%20legacy.%20I%20demand%20that%20justice%20is%20delivered%20for%20Monika%20Diamond%20and%20that%20you%20take%20a%20stand%20against%20the%20hate%20transgender%20women%20of%20color%20face%20every%20single%20day.%0D%0A%0D%0AKind%20regards%2C%0D%0A(Your%20name)%0D%0A%0D%0A
+`
+var mailtoNina = `
+ mailto:richardm@new-madrid.mo.us ?subject=Justice%20For%20Nina%20Pop&body=Dear%20sir%2C%0D%0A%0D%0AMy%20name%20is%20(your%20name)%20and%20I%20am%20writing%20to%20you%20concerning%20the%20death%20of%20Nina%20Pop.%20She%20is%20the%20fifth%20casualty%20in%20a%20four-week%20hate%20crime%20streak%20against%20transgender%20women%20of%20color.%20Nina%20Pop%20was%20stabbed%20to%20death%20in%20Sikeston%2C%20and%20from%20what%20we%20have%20heard%2C%20no%20arrests%20have%20been%20made.%0D%0A%0D%0AWhen%20transphobia%20mixes%20with%20misogyny%20and%20racism%2C%20it%20results%20in%20fatal%20violence.%20This%20amount%20of%20prejudice%20is%20unacceptable.%20Trans%20women%20of%20color%20face%20a%20disproportionately%20high%20level%20of%20violence%20against%20them%2C%20leading%20to%20the%20statistic%20that%20most%20hate%20crimes%20committed%20against%20trans%20individuals%20are%20against%20black%20trans%20women.%20I%20demand%20that%20you%20continue%20the%20investigation%20into%20Nina%20Pop%E2%80%99s%20death%20and%20ensure%20that%20justice%20is%20rightly%20delivered.%0D%0A%0D%0AYours%20sincerely%2C%0D%0A(Your%20name)%0D%0A
+`
+ 
+window.addEventListener('load', function() {
+  const params = new URLSearchParams(window.location.search)
+  var mailtoLink = ""
+  var error = false
+
+  if(!params.has('for')) {
+    document.querySelector('body').style = "background-color: red;";
+    document.querySelector('#error').hidden = false;
+    return
+  }
+  
+  person = params.get('for')
+
+  if(person == "ashanti") {
+      mailtoLink = mailtoAshanti
+  } else if(person == "bailey") {
+      mailtoLink = mailtoBailey
+  } else if(person == "beeloveslater") {
+      mailtoLink = mailtoBee
+  } else if(person == "monika") {
+      mailtoLink = mailtoMonika
+  } else if(person == "nina") {
+      mailtoLink = mailtoNina
+  } else {
+    error = true;
+  }
+
+  if(error) {
+    document.querySelector('body').style = "background-color: red;";
+    document.querySelector('#error').hidden = false;
+  } else {
+    document.querySelector('body').style = "background-color: green;";
+    document.querySelector('#success').hidden = false;
+    window.location.href = mailtoLink;
+  }
+})
